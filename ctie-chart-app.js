6f0c9f1be1ad66e67ee6dc7443c796683556e624
tys-mori <tys-mori@ctie.co.jp>
2020-11-02 10:15:53 +0900

    調整中

--- ctie-chart-app.js
+++ ctie-chart-app.js
@@ -1,13 +1,18 @@
 
 function load(file){
+  // 環境に合わせてlocationは設定する必要がある！
   var location = "http://localhost/web-components/polymer3/ctie-chart/build/es5-bundled/"
-  var script = document.createElement( 'script' );
+  var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = location+file;
   document.body.appendChild(script);
 }
-load('script1.js');
-load('script2.js');
-load(`node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js`);
-load('node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js');
-load('script3.js');
+try {
+  load('script1.js');
+  load('script2.js');
+  load('node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js');
+  load('node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js');
+  load('script3.js');
+} catch (error) {
+  console.log("loder実行エラー：", error, "※ 環境設定をチェックしてください。")
+}
